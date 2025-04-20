8007ae9dce0700805eb7149cb2171c83ab8b941c
Harsh <replytobambam@gmail.com>
2025-04-20 19:51:47 +0530

    Initalised the front End and Made the Navbar

--- Embedding.py
+++ Embedding.py
@@ -1,1 +1,24 @@
 from langchain_community.document_loaders import TextLoader
+from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEmbeddings
+import os 
+import Spliter
+
+# <------------------Imports------------------------>
+load_dotenv()
+
+os.environ('hugging_face_apikey') = os.getenv('hugging_face_apikey')
+
+
+class embedding:
+    def __init__(self,documents):
+        self.documents = documents
+        self.embedder =  HuggingFaceEmbeddings()
+
+    def embeeded(self):
+        embeded_docs = self.embedder.embed_query(self.documents)
+        return embeded_docs
+    
+
+
+
